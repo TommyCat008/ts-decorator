3123b9e1716f150ade512e5f80fdb564741c86e7
zhangyaqi04 <zhangyaqi04@baidu.com>
2023-07-10 14:47:46 +0800

    chore : 优化内容

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ATable from './ATable.vue';
+import AButton from './AButton.vue';
 import {UserEntity, UserFormEntity} from '../useHooks/usePermission.ts';
 import AForm from './AForm.vue';
 defineProps<{msg: string}>();
@@ -9,9 +9,9 @@ defineProps<{msg: string}>();
     <h1>{{ msg }}</h1>
 
     <div class="card">
-        <ATable type="ADD" text="新增用户" :userEntity="UserEntity"></ATable>
-        <ATable type="Edit" text="编辑用户" :userEntity="UserEntity"></ATable>
-        <ATable type="Delete" text="删除用户" :userEntity="UserEntity"></ATable>
+        <AButton type="ADD" text="新增用户" :userEntity="UserEntity"></AButton>
+        <AButton type="Edit" text="编辑用户" :userEntity="UserEntity"></AButton>
+        <AButton type="Delete" text="删除用户" :userEntity="UserEntity"></AButton>
         <div class="splitter"></div>
         <AForm :userEntity="UserFormEntity"></AForm>
     </div>
